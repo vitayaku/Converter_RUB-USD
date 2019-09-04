23a5f64ed1a810d09177b9a50e215e9a3fe00d0c
Виталий Якубин <vitayaku@outlook.com>
2019-09-04 22:34:37 +0300

    Создано приложено

--- Конвертер рубли-доллары/Form1.cs
+++ Конвертер рубли-доллары/Form1.cs
@@ -12,9 +12,72 @@ namespace Конвертер_рубли_доллары
 {
     public partial class Form1 : Form
     {
+        decimal rub, usd;
+
         public Form1()
         {
             InitializeComponent();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar >= '0' && e.KeyChar <= '9' )||(e.KeyChar == (char)Keys.Back))
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || (e.KeyChar == (char)Keys.Back))
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = true;
+            textBox2.Text = String.Empty;
+            textBox1.Text = String.Empty;
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Enabled = false;
+            textBox1.Enabled = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           try
+           {
+                if (radioButton1.Checked)
+                {
+                    rub = Convert.ToDecimal(int.Parse(textBox2.Text));
+                    textBox1.Text = Convert.ToString(rub /  Convert.ToDecimal(66.16));
+                }
+                else if(radioButton2.Checked)
+                {
+                    usd = Convert.ToDecimal(int.Parse(textBox1.Text));
+                    textBox2.Text = Convert.ToString(usd * Convert.ToDecimal(66.16));
+                }
+           }
+           catch
+           {
+                MessageBox.Show("Укажите сумму", "Ошибка", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+           }
         }
     }
 }
